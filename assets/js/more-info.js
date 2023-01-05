8c119b4705411a3d279751b7590ef3db6425bfac
Andre Favaro <andrevitorfavaro@gmail.com>
2023-01-05 13:15:30 -0300

    Ajuste geral na página

---
+++ assets/js/more-info.js
@@ -0,0 +1,114 @@
+let btnInfo1 = document.querySelector("#info__1");
+let btnInfo2 = document.querySelector("#info__2");
+let btnInfo3 = document.querySelector("#info__3");
+let btnInfo4 = document.querySelector("#info__4");
+let btnInfo5 = document.querySelector("#info__5");
+let btnInfo6 = document.querySelector("#info__6");
+let btnInfo7 = document.querySelector("#info__7");
+let btnInfo8 = document.querySelector("#info__8");
+let btnInfo9 = document.querySelector("#info__9");
+let btnInfo10 = document.querySelector("#info__10");
+let btnInfo11 = document.querySelector("#info__11");
+let btnInfo12 = document.querySelector("#info__12");
+let btnInfo13 = document.querySelector("#info__13");
+let btnInfo14 = document.querySelector("#info__14");
+let btnInfo15 = document.querySelector("#info__15");
+let btnInfo16 = document.querySelector("#info__16");
+
+let boxesInfo = [...document.querySelectorAll(".text__info")];
+
+btnInfo1.onclick = () => {
+  let indBtn = parseInt(btnInfo1.value);
+  boxesInfo[indBtn - 1].style.display = "inline";
+  btnInfo1.style.display = "none";
+};
+
+btnInfo2.onclick = () => {
+  let indBtn = parseInt(btnInfo2.value);
+  boxesInfo[indBtn - 1].style.display = "inline";
+  btnInfo2.style.display = "none";
+};
+
+btnInfo3.onclick = () => {
+  let indBtn = parseInt(btnInfo3.value);
+  boxesInfo[indBtn - 1].style.display = "inline";
+  btnInfo3.style.display = "none";
+};
+
+btnInfo4.onclick = () => {
+  let indBtn = parseInt(btnInfo4.value);
+  boxesInfo[indBtn - 1].style.display = "inline";
+  btnInfo4.style.display = "none";
+};
+
+btnInfo5.onclick = () => {
+  let indBtn = parseInt(btnInfo5.value);
+  boxesInfo[indBtn - 1].style.display = "inline";
+  btnInfo5.style.display = "none";
+};
+
+btnInfo6.onclick = () => {
+  let indBtn = parseInt(btnInfo6.value);
+  boxesInfo[indBtn - 1].style.display = "inline";
+  btnInfo6.style.display = "none";
+};
+
+btnInfo7.onclick = () => {
+  let indBtn = parseInt(btnInfo7.value);
+  boxesInfo[indBtn - 1].style.display = "inline";
+  btnInfo7.style.display = "none";
+};
+
+btnInfo8.onclick = () => {
+  let indBtn = parseInt(btnInfo8.value);
+  boxesInfo[indBtn - 1].style.display = "inline";
+  btnInfo8.style.display = "none";
+};
+
+btnInfo9.onclick = () => {
+  let indBtn = parseInt(btnInfo9.value);
+  boxesInfo[indBtn - 1].style.display = "inline";
+  btnInfo9.style.display = "none";
+};
+
+btnInfo10.onclick = () => {
+  let indBtn = parseInt(btnInfo10.value);
+  boxesInfo[indBtn - 1].style.display = "inline";
+  btnInfo10.style.display = "none";
+};
+
+btnInfo11.onclick = () => {
+  let indBtn = parseInt(btnInfo11.value);
+  boxesInfo[indBtn - 1].style.display = "inline";
+  btnInfo11.style.display = "none";
+};
+
+btnInfo12.onclick = () => {
+  let indBtn = parseInt(btnInfo12.value);
+  boxesInfo[indBtn - 1].style.display = "inline";
+  btnInfo12.style.display = "none";
+};
+
+btnInfo13.onclick = () => {
+  let indBtn = parseInt(btnInfo13.value);
+  boxesInfo[indBtn - 1].style.display = "inline";
+  btnInfo13.style.display = "none";
+};
+
+btnInfo14.onclick = () => {
+  let indBtn = parseInt(btnInfo14.value);
+  boxesInfo[indBtn - 1].style.display = "inline";
+  btnInfo14.style.display = "none";
+};
+
+btnInfo15.onclick = () => {
+  let indBtn = parseInt(btnInfo15.value);
+  boxesInfo[indBtn - 1].style.display = "inline";
+  btnInfo15.style.display = "none";
+};
+
+btnInfo16.onclick = () => {
+  let indBtn = parseInt(btnInfo16.value);
+  boxesInfo[indBtn - 1].style.display = "inline";
+  btnInfo16.style.display = "none";
+};
